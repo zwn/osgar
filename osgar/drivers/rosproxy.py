9c081ce7d37fd459843404f03800ae17cebc627c
Martin Dlouhy <md@robotika.cz>
2018-11-26 06:53:02 +0100

    rosproxy.py - add MyXMLRPCServer

--- osgar/drivers/rosproxy.py
+++ osgar/drivers/rosproxy.py
@@ -5,11 +5,14 @@
 from threading import Thread
 import struct
 from xmlrpc.client import ServerProxy
+from xmlrpc.server import SimpleXMLRPCServer
 
 import socket
 
 
 from osgar.bus import BusShutdownException
+
+NODE_HOST, NODE_PORT = ('127.0.0.1', 8000)
 
 
 ROS_MESSAGE_TYPES = {
@@ -29,6 +32,29 @@ def packCmdVel(speed, angularSpeed):
     return struct.pack("dddddd", speed,0,0, 0,0,angularSpeed)
 
 
+def publisherUpdate(caller_id, topic, publishers):
+    print("called 'publisherUpdate' with", (caller_id, topic, publishers))
+    return (1, "Hi, I am fine", 42) # (code, statusMessage, ignore) 
+
+def requestTopic(caller_id, topic, publishers):
+    print("REQ", (caller_id, topic, publishers))
+    return 1, "ready on martind-blabla", ['TCPROS', NODE_HOST, PUBLISH_PORT]
+
+
+class MyXMLRPCServer( Thread ):
+    def __init__( self, nodeAddrHostPort ):
+        Thread.__init__( self )
+        self.setDaemon( True )
+        self.server = SimpleXMLRPCServer( nodeAddrHostPort )
+        print("Listening on port %d ..." % nodeAddrHostPort[1])
+        self.server.register_function(publisherUpdate, "publisherUpdate")
+        self.server.register_function(requestTopic, "requestTopic")
+        self.start()
+
+    def run( self ):
+        self.server.serve_forever()
+
+
 class ROSProxy(Thread):
     def __init__(self, config, bus):
         Thread.__init__(self)
@@ -41,6 +67,7 @@ class ROSProxy(Thread):
         self.topic_type = config['topic_type']
 
     def run(self):
+        self.server = MyXMLRPCServer( (NODE_HOST, NODE_PORT) )
         master = ServerProxy(self.ros_master_uri)
         code, status_message, system_state = master.getSystemState('/')
         assert code == 1, code
