9b78110e0bc838c185e59507200f5f16c2f4c4a5
Martin Dlouhy <md@robotika.cz>
2019-10-05 17:45:08 +0200

    record.py - handle no_output from config

--- osgar/record.py
+++ osgar/record.py
@@ -22,10 +22,14 @@ class Recorder:
         buses = {}
         for module_name, module_config in config['modules'].items():
             out, slots = {}, {}
+            no_output = []
             for output_type in module_config['out']:
+                if output_type.endswith(':null'):
+                    output_type = output_type.split(':')[0]
+                    no_output.append(output_type)
                 out[output_type] = []
                 slots[output_type] = []
-            bus = BusHandler(logger, out=out, slots=slots, name=module_name)
+            bus = BusHandler(logger, out=out, slots=slots, name=module_name, no_output=no_output)
             buses[module_name] = bus
 
             module_class = module_config['driver']
