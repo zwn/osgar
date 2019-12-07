adb5655045c8c2f9ad5466001aeae21c7cf32bad
Martin Dlouhy <md@robotika.cz>
2019-12-08 00:17:22 +0100

    rospy_zmq.py - try name "tcp://*:5555"

--- subt/rospy_zmq.py
+++ subt/rospy_zmq.py
@@ -65,7 +65,7 @@ def odom2zmq():
     wait_for_master()
 
     mode = 'PUSH'  # config['mode']
-    endpoint = 'tcp://localhost:5556'  # config['endpoint']
+    endpoint = 'tcp://*:5555'  # config['endpoint']
     context = zmq.Context()
     g_socket = context.socket(zmq.PUSH)
     g_socket.connect(endpoint)
