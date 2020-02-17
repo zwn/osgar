e10b05262a147ee9172d68f1e09f9df6cb3509c1
Martin Dlouhy <md@robotika.cz>
2020-02-17 14:45:53 -0800

    eduro-view.bat - add slope lidar

--- subt/script/eduro-view.bat
+++ subt/script/eduro-view.bat
@@ -1,1 +1,1 @@
-python -m osgar.tools.lidarview --lidar lidar.scan --camera camera.raw --pose2d app.pose2d --keyframes detector.artf %*
+python -m osgar.tools.lidarview --lidar lidar.scan --lidar2 slope_lidar.scan --camera camera.raw --pose2d app.pose2d --keyframes detector.artf %*
