79ea5a2175920809704dd595adfa8da3192eae64
Martin Dlouhy <md@robotika.cz>
2020-02-20 22:19:22 -0800

    Add subt/scan_mixer.py

---
+++ subt/scan_mixer.py
@@ -0,0 +1,43 @@
+"""
+  Convert depth image to "lidar" scan
+"""
+import math
+
+import numpy as np
+
+from osgar.node import Node
+from osgar.bus import BusShutdownException
+from osgar.lib.depth import depth2dist, FX, CAMW
+from osgar.lib.mathex import normalizeAnglePIPI
+
+
+class ScanMixer(Node):
+
+    def __init__(self, config, bus):
+        super().__init__(config, bus)
+        bus.register("scan")
+        self.scan = None  # original lidar scan
+        self.slope_scan = None  # slope scan from Eduro
+        self.rs_scan = None  # RealSense RGBD converted scan from ROS
+        self.verbose = False
+
+    def update(self):
+        channel = super().update()
+        assert channel in ["scan", "slope_scan", "rs_scan"], channel
+
+        if channel == 'rs_scan':
+            pass
+        elif channel == 'scan':
+            assert len(self.scan) == 720, len(self.scan)
+            if self.rs_scan is None and self.slope_scan is None:
+                self.publish('scan', self.scan)
+                return channel  # when no other sources are available ...
+
+            # TODO some smart processing here
+            self.publish('scan', self.scan)
+        else:
+            assert False, channel  # unsupported channel
+
+        return channel
+
+# vim: expandtab sw=4 ts=4
