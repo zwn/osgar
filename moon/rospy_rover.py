3f536f63f8f8b5139207fa355ccddceb9fd40fa6
Martin Dlouhy <md@robotika.cz>
2020-04-01 21:41:46 +0200

    moon/rospy_rover.py - increase default torque to 100Nm

--- moon/rospy_rover.py
+++ moon/rospy_rover.py
@@ -178,7 +178,7 @@ def odom2zmq():
 #                vel_msg.angular.z = float(message.split(" ")[2])
 #                velocity_publisher.publish(vel_msg)
                 speed_msg.data = float(message.split(" ")[1])
-                speed_msg.data = 10.0  # HACK (Nm)
+                speed_msg.data = 100.0  # HACK (Nm)
                 vel_fl_publisher.publish(speed_msg)
                 vel_fr_publisher.publish(speed_msg)
                 speed_msg.data = 0.0  # HACK (Nm)
