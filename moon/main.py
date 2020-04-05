66901dbe428bb7e508ddb7c6421c59f698913d04
Martin Dlouhy <md@robotika.cz>
2020-04-05 21:35:31 +0200

    moon/main.py - integrate LocalPlanner

--- moon/main.py
+++ moon/main.py
@@ -8,9 +8,19 @@ from datetime import timedelta
 from osgar.node import Node
 from osgar.bus import BusShutdownException
 
+from subt.local_planner import LocalPlanner
+
 
 def distance(pose1, pose2):
     return math.hypot(pose1[0] - pose2[0], pose1[1] - pose2[1])
+
+
+def min_dist(laser_data):
+    if len(laser_data) > 0:
+        # remove ultra near reflections and unlimited values == 0
+        laser_data = [x if x > 10 else 10000 for x in laser_data]
+        return min(laser_data)/1000.0
+    return 0
 
 
 class SpaceRoboticsChallenge(Node):
@@ -19,6 +29,20 @@ class SpaceRoboticsChallenge(Node):
         bus.register("desired_speed", "artf_xyz", "pose3d", "request_origin")
         self.last_position = (0, 0, 0)  # proper should be None, but we really start from zero
         self.max_speed = 1.0  # oficial max speed is 1.5m/s
+        self.max_angular_speed = math.radians(60)
+
+        self.min_safe_dist = config.get('min_safe_dist', 0.75)
+        self.safety_turning_coeff = config.get('safety_turning_coeff', 0.8)
+        scan_subsample = config.get('scan_subsample', 1)
+        obstacle_influence = config.get('obstacle_influence', 0.8)
+        direction_adherence = math.radians(config.get('direction_adherence', 90))
+        self.local_planner = LocalPlanner(
+                obstacle_influence=obstacle_influence,
+                direction_adherence=direction_adherence,
+                max_obstacle_distance=2.5,
+                scan_subsample=scan_subsample,
+                max_considered_obstacles=100)
+
 
     def send_speed_cmd(self, speed, angular_speed):
         self.bus.publish('desired_speed', [round(speed*1000), round(math.degrees(angular_speed)*100)])
@@ -38,10 +62,32 @@ class SpaceRoboticsChallenge(Node):
                 break
         self.send_speed_cmd(0.0, 0.0)
 
+    def go_safely(self, desired_direction):
+        safety, safe_direction = self.local_planner.recommend(desired_direction)
+        desired_angular_speed = 0.9 * safe_direction
+        size = len(self.scan)
+        dist = min_dist(self.scan[size//3:2*size//3])
+        if dist < self.min_safe_dist:
+            desired_speed = self.max_speed * (dist - self.dangerous_dist) / (self.min_safe_dist - self.dangerous_dist)
+        else:
+            desired_speed = self.max_speed
+        desired_speed = desired_speed * (1.0 - self.safety_turning_coeff * min(self.max_angular_speed, abs(desired_angular_speed)) / self.max_angular_speed)
+        self.send_speed_cmd(desired_speed, desired_angular_speed)
+        return safety
+
+    def random_walk(self, timeout):
+        start_time = self.time
+        while self.time - start_time < timeout:
+            if self.update() == 'scan':
+                self.go_safely(0.0)
+
+        self.send_speed_cmd(0.0, 0.0)
+
     def run(self):
         try:
             self.update()  # define self.time
-            self.go_straight(2.0, timeout=timedelta(seconds=10))
+#            self.go_straight(2.0, timeout=timedelta(seconds=10))
+            self.random_walk(timeout=timedelta(seconds=10))
         except BusShutdownException:
             pass
 
