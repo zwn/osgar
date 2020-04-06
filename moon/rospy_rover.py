29ffc9c45c58aa21fe5c5dd2cec5ce48233ca014
Martin Dlouhy <md@robotika.cz>
2020-04-06 22:04:37 +0200

    moon/rospy_rover.py - add steering based on desired_angular_speed

--- moon/rospy_rover.py
+++ moon/rospy_rover.py
@@ -4,6 +4,7 @@
 """
 import time
 import struct
+import math
 from io import BytesIO
 
 import zmq
@@ -20,6 +21,11 @@ from srcp2_msgs.msg import qual_1_scoring_msg, vol_sensor_msg
 
 
 ROBOT_NAME = 'scout_1'
+
+WHEEL_SEPARATION_WIDTH = 1.87325  # meters
+WHEEL_SEPARATION_HEIGHT = 1.5748  # meters
+
+
 FILTER_ODOM_NTH = 1  #n - every nth message shall be sent to osgar
 FILTER_CAMERA_NTH = 4 #n - every nth message shall be sent to osgar
 FILTER_DEPTH_NTH = 1  #n - every nth message shall be sent to osgar
@@ -178,12 +184,20 @@ def odom2zmq():
     speed_msg = Float64()
     speed_msg.data = 0
 
+    steering_msg = Float64()
+    steering_msg.data = 0
+
     # /name/fl_wheel_controller/command
     QSIZE = 10
     vel_fl_publisher = rospy.Publisher('/scout_1/fl_wheel_controller/command', Float64, queue_size=QSIZE)
     vel_fr_publisher = rospy.Publisher('/scout_1/fr_wheel_controller/command', Float64, queue_size=QSIZE)
     vel_bl_publisher = rospy.Publisher('/scout_1/bl_wheel_controller/command', Float64, queue_size=QSIZE)
     vel_br_publisher = rospy.Publisher('/scout_1/br_wheel_controller/command', Float64, queue_size=QSIZE)
+
+    steering_fl_publisher = rospy.Publisher('/scout_1/fl_steering_arm_controller/command', Float64, queue_size=QSIZE)
+    steering_fr_publisher = rospy.Publisher('/scout_1/fr_steering_arm_controller/command', Float64, queue_size=QSIZE)
+    steering_bl_publisher = rospy.Publisher('/scout_1/bl_steering_arm_controller/command', Float64, queue_size=QSIZE)
+    steering_br_publisher = rospy.Publisher('/scout_1/br_steering_arm_controller/command', Float64, queue_size=QSIZE)
 
     r = rospy.Rate(10)
     while True:
@@ -204,6 +218,29 @@ def odom2zmq():
                 vel_fr_publisher.publish(speed_msg)
                 vel_bl_publisher.publish(speed_msg)
                 vel_br_publisher.publish(speed_msg)
+
+                desired_speed = float(message.split(" ")[1])
+                desired_angular_speed = float(message.split(" ")[2])
+                if abs(desired_speed) > 0.001:
+                    if abs(desired_angular_speed) > 0.001:
+                        # i.e. turn and go
+                        radius = desired_speed/desired_angular_speed
+                        angle_left = math.atan2(WHEEL_SEPARATION_HEIGHT/2.0, radius - WHEEL_SEPARATION_WIDTH/2.0)
+                        angle_right = math.atan2(WHEEL_SEPARATION_HEIGHT/2.0, radius + WHEEL_SEPARATION_WIDTH/2.0)
+                    else:
+                        angle_left = 0.0
+                        angle_right = 0.0
+
+                    steering_msg.data = angle_left
+                    steering_fl_publisher.publish(steering_msg)
+                    steering_msg.data = angle_right
+                    steering_fr_publisher.publish(steering_msg)
+                    steering_msg.data = -angle_left
+                    steering_bl_publisher.publish(steering_msg)
+                    steering_msg.data = -angle_right
+                    steering_br_publisher.publish(steering_msg)
+                else:
+                    pass  # keep steering angles as they are ...
 
         except zmq.error.Again:
             pass
