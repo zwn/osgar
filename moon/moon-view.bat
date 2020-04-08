ba30428faeb1695bfac34f54e89b491b5998ae03
Martin Dlouhy <md@robotika.cz>
2020-04-08 12:06:54 +0200

    moon/moon-view.bat - use app.pose2d

--- moon/moon-view.bat
+++ moon/moon-view.bat
@@ -1,1 +1,1 @@
-python -m osgar.tools.lidarview --pose2d rosmsg.pose2d --lidar rosmsg.scan --camera rosmsg./scout_1/camera/left/image_raw/compressed --camera2 rosmsg./scout_1/camera/right/image_raw/compressed %*
+python -m osgar.tools.lidarview --pose2d app.pose2d --lidar rosmsg.scan --camera rosmsg./scout_1/camera/left/image_raw/compressed --camera2 rosmsg./scout_1/camera/right/image_raw/compressed %*
