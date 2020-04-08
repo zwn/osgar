f27d4c944e5c8a36e8c047bebd2dbad64a993262
Martin Dlouhy <md@robotika.cz>
2020-04-08 22:38:15 +0200

    moon/rospy_rover.py - hack print current thread

--- moon/rospy_rover.py
+++ moon/rospy_rover.py
@@ -7,6 +7,7 @@ import struct
 import math
 from io import BytesIO
 from threading import RLock
+import threading  # just hacking rospy
 
 import zmq
 
@@ -41,6 +42,7 @@ g_lock = RLock()
 
 
 def socket_send(data):
+    print('Thread', threading.current_thread())
     global g_socket, g_lock
     assert g_socket is not None
     with g_lock:
